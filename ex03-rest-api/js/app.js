c07af25e2678417bf7b4a02a79edb8ff78423d3e
서민정 <ybsm0629@gmail.com>
2024-12-16 14:39:52 +0900

    API

--- ex03-rest-api/js/app.js
+++ ex03-rest-api/js/app.js
@@ -42,17 +42,44 @@ document.addEventListener("DOMContentLoaded", function () {
             })
     }
 
+
+
     // Add new TODO
     //  새로운 todo를 추가하는 이벤트 핸들러 연결
     addTodoButton.addEventListener("click", (event) => {
-        //
+        // #new-todo 입력 상자의 값을 가지고 와서
+        // 새 todo item 항목을 만들어서 서버로 전송 (POST)
+        const title = newTodoInput.value.trim();
+        if (title) {
+            // 서버로 전송
+            fetch(apiUrl, {
+                method: "POST",     // 생성
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title: title,
+                    completed: false
+                })
+            })
+                .then((response) => response.json())
+                .then(todo => {
+                    addTodoToDOM(todo);
+                    newTodoInput.value = "";
+                })
+                .catch(error => console.error("Error adding TODO:", error));
+        }
+        newTodoInput.focus();
     });
 
+
+
+    // 3. 
     // Delete TODO
     //  특정 todo를 삭제하는 함수 (삭제 버튼의 클릭 이벤트 핸들러로 사용됨)
     window.deleteTodo = function (id) {
         // Endpoint: http://localhost:3000/todos/{id}
-        fetch(`${apiUrl}/${id}`, {
+        fetch(`${apiUrl}/${id}`, {         // 서버에 데이터 요청하는 부분. apiUrl = 서버의 기본 주소, /id = 요청하려는 데이터의 고유 식별자인 ID 나타내는 부분
             method: "DELETE"
         })
             .then(() => {
